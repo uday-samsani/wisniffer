83632ff462c1b6550961605caa607853690401f0
root <root@localhost.localdomain>
2019-03-03 23:08:16 +0530

    added clients scan

--- wisniffer.py
+++ wisniffer.py
@@ -1,37 +1,47 @@
-#!/bin/python3
+#!/usr/bin/python3
 import argparse
 import subprocess
 import re
 import random
 from scapy.all import *
 
+clients = []
+
 macRegExp = re.compile(r'([0-9A-Fa-f]{2}:){5}([0-9A-Fa-f]{2})')
 
 
 def main():
-    parser = argparse.ArgumentParser(
-        description='''Wi-Sniffer sniffs the data, packets from access
-                       points.'''
-    )
-    parser.add_argument('-s', '--scan', action='store_true',
-                        help='Scan for Access Points in range')
-    parser.add_argument('-b', '--bssid', type=macaddr, metavar='',
-                        help='BSSID i.e. MAC address of an access point')
-    parser.add_argument('-e', '--essid', type=str, metavar='',
-                        help='ESSID i.e. access point name')
-    parser.add_argument('-i', '--iface', type=str, metavar='', required=True,
-                        help='Interface to sniff')
-    parser.add_argument('-d', '--deauth', type=int, metavar='',
-                        help='Deauth probe to a AP or a client')
-    parser.add_argument('-t', '--timeout', type=int, default=10,
-                        help='Timeout for sniffing( Default = 10)')
-    parser.add_argument('-o', '--output', type=str, metavar='',
-                        help='Output to a file')
-    parser.add_argument('-v', '--verboose', action='store_true',
-                        help='Verboose output')
 
+    # Command-Line arguments
+    parser = argparse.ArgumentParser(description='''WiSniffer is an wireless info gathering and snffing tool.
+         Scan for Access Points and perform scans, deauthentications etc..,''')
+    networkOpt = parser.add_argument_group('Network Options')
+    networkOpt.add_argument('-b', '--bssid', type=macaddr, metavar='',
+                            help='BSSID of an AP')
+    networkOpt.add_argument('-e', '--essid', type=str, metavar='',
+                            help='ESSID of an AP')
+    networkOpt.add_argument('-c', '--channel', type=int, metavar='',
+                            help='Channel of AP')
+    networkOpt.add_argument('-i', '--iface', type=str, metavar='',
+                            help='Interface to use for scanning')
+    scanOpt = parser.add_argument_group('Scan Options')
+    scanOpt.add_argument('-s', '--scan', action='store_true',
+                         help='Scan all AP\'s in range')
+    scanOpt.add_argument('-d', '--deauth', action='store_true',
+                         help='Send de-authentication frames to AP')
+    scanOpt.add_argument('-t', '--timeout', type=int,
+                         help='Timeout for sniffing( Default = 10)')
+    extraOpt = parser.add_argument_group('Extra Options')
+    extraOpt.add_argument('-o', '--output', type=str, metavar='',
+                          help='Output to a file')
+    printOpt = extraOpt.add_mutually_exclusive_group()
+    printOpt.add_argument('-q', '--quiet', action='store_true',
+                          help='Scan quietly')
+    printOpt.add_argument('-v', '--verboose', action='store_true',
+                          help='print verboose')
     global args
     args = parser.parse_args()
+
     proc = subprocess.run(['ip', 'link', 'show'], stdout=subprocess.PIPE)
     output = proc.stdout.decode('utf-8')
     output = output.split('\n')
@@ -91,28 +101,31 @@ def scanAp():
             sniff(iface=args.iface, count=1, prn=scanApPkt,
                   timeout=args.timeout)
         except KeyboardInterrupt:
-            break
+            sys.exit(0)
         i = i+1
     else:
         print('while is over')
 
 
 def scanClients():
-    if args.bssid:
-        while True:
-            sniff(iface=args.iface, count=1, prn=scanClientsPkt)
+    subprocess.run(['iw', 'dev', args.iface, 'set', 'channel',
+                    str(args.channel)])
+    if not args.quiet:
+        print('BSSID: {0} CHANNEL: {1}'.format(args.bssid, args.channel))
+    while True:
+        try:
+            sniff(iface=args.iface, count=10, prn=scanClientsPkt)
+        except KeyboardInterrupt:
+            sys.exit(0)
 
 
 def scanClientsPkt(pkt):
-    global clients
-    clients = []
     if pkt.haslayer(Dot11):
-        if pkt.type == 2 and pkt.haslayer(EAPOL):
-            print(pkt.addr2 + ' ' + pkt.addr1)
-            if (pkt.addr1 not in clients and pkt.addr2 == args.bssid):
+        pkt = pkt.getlayer(Dot11)
+        if pkt.type == 2:
+            if ((pkt.addr2 not in clients) and (pkt.addr1 == args.bssid)):
                 clients.append(pkt.addr2)
-                print('{0} {1}'.format(len(clients),
-                                       pkt.addr2))
+                print('{0} {1}'.format(len(clients), pkt.addr2))
 
 
 if __name__ == '__main__':
